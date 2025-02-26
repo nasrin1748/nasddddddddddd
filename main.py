7f275849a8abb1a26aec66d35a79fb4ff8696c82
nasrin pathan <79505893+nasrin1748@users.noreply.github.com>
2025-02-26 17:42:20 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-# idle_shell.py (This will be embedded, so no need for a separate file)
-import code
-
-def main():
-    # Starting the interactive shell
-    print(code.interact())
-
-main()
+def fun():
+     import code
+     print(code.interact())
+fun()
