bc0a201661db250c4f771efcbb08969e22e5934a
nasrin pathan <79505893+nasrin1748@users.noreply.github.com>
2025-02-26 17:33:25 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 def fun():
     import code
+    import subprocess
     import sys
 
     # Install the package (e.g., requests)
-    check_call([sys.executable, "-m", "pip", "install", "pandas"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "pandas","matplotlib","zobi","buckaroo"])
 
     # Start an interactive session
-    print(code.interact())
+    print(code.interact(local=locals()))
 
 fun()
